8a2cd467e64895adf55ae766111012242b1edc6e
seunghw <ysp1307@naver.com>
2021-09-23 22:32:28 +0900

    시계파트 주석

--- js/clock.js
+++ js/clock.js
@@ -1,7 +1,8 @@
-const clock = document.querySelector("h2#clock");
+const clock = document.querySelector("h2#clock"); // 시계 파트
 
 function getClock() {
-  const date = new Date();
+  // 날짜 불러오기
+  const date = new Date(); //새로운 날짜 생성
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
@@ -9,4 +10,4 @@ function getClock() {
 }
 
 getClock();
-setInterval(getClock, 1000); // 매 ~마다 실행
+setInterval(getClock, 1000); // 매 1초마다 실행
