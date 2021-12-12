43604188d60904714d332a2d08e70c6928d33cc0
seunghw <ysp1307@naver.com>
2021-12-12 16:49:24 +0900

    css 스타일링 및 이미지 추가

--- js/clock.js
+++ js/clock.js
@@ -1,13 +1,23 @@
-const clock = document.querySelector("h2#clock"); // 시계 파트
+const clock = document.querySelector("#clock"); // 시계 파트
+const todayInfo = document.querySelector("#today-info"); // 날짜 파트
 
-function getClock() {
+function getToday() {
   // 날짜 불러오기
   const date = new Date(); //새로운 날짜 생성
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
   const seconds = String(date.getSeconds()).padStart(2, "0");
-  clock.innerText = `${hours}:${minutes}:${seconds}`;
+
+  const yy = date.getFullYear();
+  const mm = date.getMonth() + 1;
+  const dd = date.getDate();
+  const days = date.getDay();
+
+  const week = ["일", "월", "화", "수", "목", "금", "토"];
+
+  clock.innerText = `${hours} : ${minutes} : ${seconds}`;
+  todayInfo.innerText = `${yy}년 ${mm}월 ${dd}일 ${week[days]}요일`;
 }
 
-getClock();
-setInterval(getClock, 1000); // 매 1초마다 실행
+getToday();
+setInterval(getToday, 1000); // 매 1초마다 실행
