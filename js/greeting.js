75b8c55848e0f4daa9b43981ffe82461a2ec61a2
seunghw <ysp1307@naver.com>
2021-12-14 23:59:33 +0900

    로그인 전 header 제거 추가

--- js/greeting.js
+++ js/greeting.js
@@ -1,6 +1,7 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const header = document.getElementById("header");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -10,11 +11,11 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-
   paintGreeting(username);
 }
 
 function paintGreeting(username) {
+  header.style.display = "flex";
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
